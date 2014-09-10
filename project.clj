04b1e04a92155167850b7579f75190be909d731f
Jaap ter Woerds <jaap@siteworkers.nl>
2014-09-10 10:15:11 +0200

    Update version

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [riemann             "0.2.4"]
-                 [clj-kafka           "0.1.2-0.8"
+                 [clj-kafka           "0.2.6-0.8"
                   :exclusions [org.slf4j/slf4j-log4j12
                                org.slf4j/slf4j-simple]]])
