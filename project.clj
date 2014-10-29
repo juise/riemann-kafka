4d43a6e5f24a3081997613a4fa2c0d31eae080da
Jens Henrik Hertz <jens@treatwell.nl>
2014-10-29 17:39:55 +0100

    Remove riemann and clojure from the generated uberjar

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/pyr/riemann-kafka"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [riemann             "0.2.6"]
-                 [clj-kafka           "0.2.6-0.8"
+  :profiles { :provided { :dependencies [[org.clojure/clojure "1.6.0"], [riemann "0.2.6"]] } }
+  :dependencies [[clj-kafka           "0.2.6-0.8"
                   :exclusions [org.slf4j/slf4j-log4j12
-                               org.slf4j/slf4j-simple]]])
+                               org.slf4j/slf4j-simple
+                               org.clojure/clojure]]])
